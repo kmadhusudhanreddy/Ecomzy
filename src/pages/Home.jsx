a0a83df955781adad326ea5b1e19086e0ab69cf1
kmadhusudhanreddy <kmadhureddy8688@gmail.com>
2024-09-10 17:12:14 +0530

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
+
+const Home = () => {
+  const API_URL = "https://fakestoreapi.com/products";
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  async function fetchProductData() {
+    setLoading(true);
+
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+
+      setPosts(data);
+    } catch (error) {
+      console.log("Error aagya ji");
+      setPosts([]);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : posts.length > 0 ? (
+        <div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-5xl p-2 mx-auto space-y-8 space-x-5 min-h-[40vh] mb-10">
+          {posts.map((post) => (
+            <Product key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center">
+          <p>No Data Found</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
